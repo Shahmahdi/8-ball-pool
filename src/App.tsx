7690d9697c1f6b51f999b8721240d1096ae71743
mahdi <shahmahdi11@gmail.com>
2019-11-10 19:07:04 +0600

    borad backgorund added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+const mainBoard = require('./resources/spr_background4.png');
 
 const App: React.FC = () => {
+
+  const canvasRef = React.useRef(null);
+
+  useEffect(() => {
+    let canvas: any = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    let image = new Image();
+    image.src = mainBoard;
+    
+    setTimeout(() => {
+      ctx.drawImage(image, 0, 0, 800, 400);
+    }, 1000);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas 
+        ref={canvasRef}
+        width='800'
+        height='400'
+      />
     </div>
   );
 }
